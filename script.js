17e09625fa1d07615bf6b69f0122e353046fad91
Shivavigneshkannan-k <shivavigneshml@gmail.com>
2024-06-21 22:57:55 +0530

    properly comparison add. Issue fixed

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-const audio= document.querySelectorAll("audio");
 
 
-const play=(search)=>{
-    audio.forEach(e=>{
-        // to fix => the comparison is not efficient at all
-        let selected = e.dataset[key="key"];
-        selected=String.fromCharCode(selected);
-        selected=selected.toLowerCase();
-
-        if(selected==search){
-            e.currentTime=0;
-            e.play();
-            selected= selected.toUpperCase();
-            const key = document.getElementById(`${selected}`);
-
-            key.classList.add("playing");
-            key.addEventListener("transitionend",e=>{
-                if(e.propertyName=="transform"){
-                    key.classList.remove("playing");
-                }
-            })
+function play(obj){
+    const audio = document.querySelector(`audio[data-key="${obj.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${obj.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add("playing");
+    key.addEventListener("transitionend",e=>{
+        if(e.propertyName=="transform"){
+            key.classList.remove("playing");
         }
-
     })
 }
 
 
 window.addEventListener("keydown",e =>{
-    play(e.key);
+    play(e);
 });
